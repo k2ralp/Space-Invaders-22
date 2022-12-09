20d2de418c785e9da484c7433bb99ad70f72d0a2
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2022-12-10 01:32:14 +0200

    Add comments and increase player speed

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,8 +4,6 @@ public class PlayerController : MonoBehaviour
 {
     public SpriteRenderer playerSprite;
     public float moveSpeed = 2f;
-    float minX = -8.6f;
-    float maxX = 8.6f;
 
     private Camera mainCamera;
     private float rightScreenEdge;
@@ -35,23 +33,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Store the input value of the left-right direction buttons
         float hlInput = Input.GetAxis("Horizontal");
+
+        // Calculate and store the max limits for the player to move
         float rightLimit = rightScreenEdge - playerSpriteHalfWidth;
         float leftLimit = leftScreenEdge + playerSpriteHalfWidth;
+
+        // Store the player's current position
         Vector2 currentPos = transform.position;
 
+        // If the player pressed right direction AND they're to the left side of the right limit, keep moving
         if (hlInput > 0f && transform.position.x < rightLimit)
         {
-            
+            // Calculate the player's new position
             Vector2 newPos = currentPos + new Vector2(1f, 0f);
 
+            // Move the player towards the new position using the given speed
             transform.position = Vector2.MoveTowards(currentPos, newPos, moveSpeed * Time.deltaTime);
         }
+        // If the player pressed left direction AND they're to the right side of the left limit, keep moving
         else if (hlInput < 0f && transform.position.x > leftLimit)
         {
-            //Vector2 currentPos = transform.position;
+            // Same as above but new position is the opposite direction
             Vector2 newPos = currentPos - new Vector2(1f, 0f);
-
             transform.position = Vector2.MoveTowards(currentPos, newPos, moveSpeed * Time.deltaTime);
         }
     }
