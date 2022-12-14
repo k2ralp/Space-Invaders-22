85fbbbdd99a482638e8b7573c168303965918183
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2022-12-15 00:28:49 +0200

    Add collision detection between projectiles and aliens

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    public AudioClip clip;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,23 @@ public class EnemyBehaviour : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the other object is the projectile by its tag
+        if (collision.tag == "Laser")
+        {
+            print("I'm Hit");
+
+            // Allows playing an audio clip even if the Alien is destroyed and removed from the scene
+            AudioSource.PlayClipAtPoint(clip, Vector2.zero);
+
+            // Destroy the Alien game object (the one this script is on)
+            Destroy(gameObject);
+
+            // Destroy the projectile game object
+            Destroy(collision.gameObject);
+        }
     }
 }
--- Assets/Scripts/Projectile/ProjectileBehaviour.cs
+++ Assets/Scripts/Projectile/ProjectileBehaviour.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class ProjectileBehaviour : MonoBehaviour
 {
     public float speed;
+    public float destroyAfter = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Option 1 - Destroy the projectile after some time has passed
+        //Destroy(gameObject, destroyAfter);
+    }
 
+    // Option 2 - Destroy the projectile if it's outside the camera view
+    // (only works if the object has a Sprite renderer (2D) or Mesh renderer (3D) components)
+    void OnBecameInvisible()    
+    {
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
