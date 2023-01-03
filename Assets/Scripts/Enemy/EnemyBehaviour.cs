58d7d4b24179eb494dcc7359f1a478768b7e3862
Kadir Alp Yaltı <kadiralpyalti@gmail.com>
2023-01-04 03:03:38 +0400

    replace bgm

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -6,16 +6,29 @@ public class EnemyBehaviour : MonoBehaviour
 {
     public AudioClip clip;
 
+    public class Enemy
+    {
+
+        public int enemyCount;
+        public Enemy(int amount)
+        {
+            enemyCount = amount;
+        }
+
+    }
+
+    public Enemy enemyLeft = new Enemy(36);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -23,7 +36,10 @@ public class EnemyBehaviour : MonoBehaviour
         // Check if the other object is the projectile by its tag
         if (collision.tag == "Laser")
         {
+            ReduceEnemy();
+            
             print("I'm Hit");
+            print(enemyLeft.enemyCount + "Enemy Left");
 
             // Allows playing an audio clip even if the Alien is destroyed and removed from the scene
             AudioSource.PlayClipAtPoint(clip, Vector2.zero);
@@ -34,5 +50,10 @@ public class EnemyBehaviour : MonoBehaviour
             // Destroy the projectile game object
             Destroy(collision.gameObject);
         }
+    }
+
+    void ReduceEnemy()
+    {
+        enemyLeft.enemyCount = enemyLeft.enemyCount - 1;
     }
 }
